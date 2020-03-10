99db6d594af80e7e09d2760513b93e71867cca00
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2020-03-10 07:57:03 -0700

    Agregar archivos de proyecto.

---
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -0,0 +1,171 @@
+// PlantillaGraficos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define GLEW_STATIC
+
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+#include <math.h>
+
+#include <iostream>
+
+#include "Shader.h"
+#include "Vertice.h"
+#include <vector>
+
+using namespace std;
+
+//Cada elemento que queramos renderear necesita 
+//Un vertex array y un buffer
+vector<Vertice> triangulo;
+GLuint vertexArrayTrianguloID;
+GLuint bufferTrianguloID;
+
+//Instancia del shader
+Shader *shader;
+//Identificadores para mapeo de atributos de entrada
+//del vertex shader
+GLuint posicionID;
+GLuint colorID;
+
+
+void inicializarTriangulo() {
+	Vertice v1 = {
+		vec3(0.0f,0.3f,0.0f),
+		vec4(0.8f,0.1f,0.0f,1.0f)
+	};
+	Vertice v2 = {
+		vec3(-0.3f,-0.3f,0.0f),
+		vec4(0.8f,0.1f,0.0f,1.0f)
+	};
+	Vertice v3 = {
+		vec3(0.3f,-0.3f,0.0f),
+		vec4(0.8f,0.1f,0.0f,1.0f)
+	};
+	triangulo.push_back(v1);
+	triangulo.push_back(v2);
+	triangulo.push_back(v3);
+}
+
+void dibujar() {
+	//Elegir shader
+	shader->enlazar();
+	//Elegir el vertex array
+	glBindVertexArray(vertexArrayTrianguloID);
+	//Dibujar
+	glDrawArrays(GL_TRIANGLES, 0, triangulo.size());
+
+	//Soltar vertex array
+	glBindVertexArray(0);
+	//Desenlazar shader
+	shader->desenlazar();
+}
+
+int main()
+{
+    //Declarar una ventana
+	GLFWwindow* window;
+
+	//Si no se pudo iniciar GLFW
+	//Terminamos ejecucion
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+	//Si se pudo iniciar GLFW
+	//inicializamos la ventana
+	window =
+		glfwCreateWindow(600, 600, "Ventana",
+			NULL, NULL);
+	//Si no se pudo crear la venata
+	//Terminamos ejecucion
+	if (!window) {
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+	//Establecemos la ventana como contexto
+	glfwMakeContextCurrent(window);
+
+	//Una vez establecido el contexto
+	//Se activan las funciones "modernas" (gpu)
+	glewExperimental = true;
+
+	GLenum errores = glewInit();
+	if (errores != GLEW_OK) {
+		glewGetErrorString(errores);
+	}
+
+	const GLubyte* versionGL =
+		glGetString(GL_VERSION);
+	cout << "Version OpenGL: " << versionGL;
+
+	inicializarTriangulo();
+
+	const char* rutaVertexShader =
+		"VertexShader.shader";
+	const char* rutaFragmentShader =
+		"FragmentShader.shader";
+	shader = new Shader(rutaVertexShader, rutaFragmentShader);
+
+	//Mapeo de atributos
+	posicionID =
+		glGetAttribLocation(shader->getID(), "posicion");
+	colorID =
+		glGetAttribLocation(shader->getID(), "color");
+
+	shader->desenlazar();
+
+	//Crear el vertex array del triangulo
+	glGenVertexArrays(1, &vertexArrayTrianguloID);
+	glBindVertexArray(vertexArrayTrianguloID);
+	//Vertex buffer
+	glGenBuffers(1, &bufferTrianguloID);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferTrianguloID);
+	//Asociar datos al buffer
+	glBufferData(GL_ARRAY_BUFFER,
+		sizeof(Vertice) * triangulo.size(),
+		triangulo.data(), GL_STATIC_DRAW);
+	//Habilitar atributos de shader
+	glEnableVertexAttribArray(posicionID);
+	glEnableVertexAttribArray(colorID);
+	//Especificar a OpenGL como comunicarse
+	glVertexAttribPointer(posicionID,
+		3, GL_FLOAT, GL_FALSE,
+		sizeof(Vertice), 0);
+	glVertexAttribPointer(colorID,
+		4, GL_FLOAT, GL_FALSE,
+		sizeof(Vertice), (void*)sizeof(vec3));
+
+	//Soltar el vertex array y el buffer
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+
+
+
+	//Ciclo de dibujo (Draw loop)
+	while (!glfwWindowShouldClose(window)) {
+		//Establecer region de dibujo
+		glViewport(0, 0, 600, 600);
+		//Establecemos el color de borrado
+		//Valores RGBA
+		glClearColor(1, 0.8, 0, 1);
+		//Borrar!
+		glClear(GL_COLOR_BUFFER_BIT | 
+			GL_DEPTH_BUFFER_BIT);
+
+		//Actualizar valores y dibujar
+		dibujar();
+
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+	//Despúes del ciclo de dibujo
+	//Eliminamos venta y procesos de glfw
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
